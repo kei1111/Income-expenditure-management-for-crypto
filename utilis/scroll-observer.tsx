e0c829e7ba78ae498430c071b49b8de450cc269c
kei1111 <ke1taki6a25@outlook.jp>
2022-04-18 23:14:25 +0900

    initial commit

---
+++ utilis/scroll-observer.tsx
@@ -0,0 +1,39 @@
+import React, {
+  useEffect,
+  useContext,
+  useState,
+  useCallback,
+  ReactNode,
+} from "react";
+
+interface ScrollValue {
+  scrollY: number;
+}
+
+interface ScrollProps {
+  children: ReactNode;
+}
+
+export const ScrollContext = React.createContext<ScrollValue>({
+  scrollY: 0,
+});
+
+const ScrollObserver = ({ children }: ScrollProps) => {
+  const [scrollY, setScrollY] = useState(0);
+  const handleScroll = useCallback(() => {
+    setScrollY(window.scrollY);
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("scroll", handleScroll, { passive: true });
+    return () => document.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
+
+  return (
+    <ScrollContext.Provider value={{ scrollY }}>
+      {children}
+    </ScrollContext.Provider>
+  );
+};
+
+export default ScrollObserver;
