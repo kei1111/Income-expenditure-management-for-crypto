2654ddde8b088a7d20032fd5cb56c4399b58f5f5
kei1111 <ke1taki6a25@outlook.jp>
2022-08-27 17:16:03 +0900

    fix masthead

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useContext, useState, useCallback } from "react";
 import Image from "next/image";
 import { ScrollContext } from "../utilis/scroll-observer";
+import Link from "next/link";
 
 const Masthead: React.FC = () => {
   const [imageLoaded, setImageLoaded] = useState(false);
@@ -18,32 +19,22 @@ const Masthead: React.FC = () => {
   }, []);
 
   return (
-    <div
-      ref={refContainer}
-      className="min-h-screen flex flex-col sticky top-0 -z-10"
-      style={{
-        transform: `translateY(-${progress * 20}vh)`,
-      }}
-      
-    >    
+    <div className="fv-container">
+      <div className="fv-text">
+        <p>STEPNの管理をこのアプリ１つで簡単に。</p>
+        <h1>クリプトノート</h1>
 
-
-      <div className="fv-container">
-
-     
-
-        <div className="fv-text">
-          <p>STEPNの管理をこのアプリ一つで簡単に。</p>
-          <h1>クリプトノート</h1>
-
-          <div className="app_icon"> 
-            <img src="assets/app_icon.png" width="200"></img>
-          </div> 
-        </div> 
-
-        <div className="fv-gazo">
-          <img src="assets/fv_gazo.png"></img>
+        <div className="app_icon">
+          <Link href="/terms">
+            <a>
+              <img src="assets/app_icon.png" width="200"></img>
+            </a>
+          </Link>
         </div>
+      </div>
+
+      <div className="fv-gazo">
+        <img src="assets/fv_gazo.png"></img>
       </div>
     </div>
   );
